bb6da1abb5b4217fd7f2a1b1d3a8bb4c24f966ba
Tobias <tobias.bartsch@telekom.de>
2018-11-08 15:44:10 +0100

    initial commit

---
+++ src/main/java/group/project/riskmanagementBE/RiskmanagementBeApplication.java
@@ -0,0 +1,12 @@
+package group.project.riskmanagementBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskmanagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiskmanagementBeApplication.class, args);
+	}
+}
